5fd068e84686258728478958c97635186bd02bf1
Sain <hussein.rahal11@gmail.com>
2021-03-25 21:22:33 -0400

    tkinter
    first mess around with tkinter

---
+++ src/main.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = miles * 1.609
+    km_result.config(text=f"{km}")
+    return km
+
+
+window = Tk()
+window.title("Miles to Km Converter")
+window.config(padx=20, pady=20)
+
+
+miles_input = Entry(width=7)
+miles_input.grid(column=1, row=0)
+
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+equal_to_label = Label(text="is equal to")
+equal_to_label.grid(column=0, row=1)
+
+km_result = Label(text="0")
+km_result.grid(column=1, row=1)
+
+km_label = Label(text="Km")
+km_label.grid(column=2, row=1)
+
+button = Button(text="Calculate", command=miles_to_km)
+button.grid(column=1, row=2)
+
+
+window.mainloop()
